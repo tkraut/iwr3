27837944fa184bc35c22ed13f03431cb66f4afce
tk <tk@ecb85b3f-eceb-4b58-b4ef-d6a9b56e6719>
2011-05-26 18:05:19 +0000

    package info repaired  ant buildfile added  documentation changed
    git-svn-id: https://chlup.info/svn/mazlicek/iwr@164 ecb85b3f-eceb-4b58-b4ef-d6a9b56e6719

--- src/iwr/package-info.java
+++ src/iwr/package-info.java
@@ -1,8 +1,8 @@
 /**
 <p>Tento balíček obsahuje celý přehrávač záznamů.</p>
 
-<p>Pro zobrazení používám knihovnu Swing {@see javax.Swing}</p>
-<p>Pro načítání XML DOM {@see org.w3c.dom}</p>
+<p>Pro zobrazení používám knihovnu Swing ({@literal javax.Swing})</p>
+<p>Pro načítání XML používám DOM ({@literal org.w3c.dom})</p>
 @author Tomáš Kraut
 */
 package iwr;
